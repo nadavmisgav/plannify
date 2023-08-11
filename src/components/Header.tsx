20987b0a6d2e37ce0c810a6458d12747830c025e
ned3144 <nadav.misgav@gmail.com>
2023-08-11 13:21:26 +0300

    fix hr

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,25 @@ import Logo from '../../assets/plannify.svg';
 
 export default function Header() {
   return (
-    <div className="flex items-center">
-      <Link to="/">
-        <img src={Logo} alt="plannify logo" width="100%" height="auto" />
-      </Link>
-      <nav className="ml-auto">
-        <ul className="flex justify-between p-4 px-8 items-center">
-          <li>
-            <Link className="uppercase tracking-widest font-bold" to="/project">
-              projects
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <hr />
-    </div>
+    <>
+      <div className="flex items-center py-2">
+        <Link to="/">
+          <img src={Logo} alt="plannify logo" width="100%" height="auto" />
+        </Link>
+        <nav className="ml-auto">
+          <ul className="flex justify-between p-4 px-8 items-center">
+            <li>
+              <Link
+                className="uppercase tracking-widest font-bold"
+                to="/project"
+              >
+                projects
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <hr className="border-[1px] my-2 border-cyan-200" />
+    </>
   );
 }
