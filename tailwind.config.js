c5ba8cb448f0351b32a3005ccab34c26b2bc59cd
ned3144 <nadav.misgav@gmail.com>
2023-08-11 13:21:21 +0300

    add color template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {},
+  theme: {
+    colors: {
+      cyan: {
+        100: '#f8f7f4',
+        200: '#66cbc4',
+        300: '#69bab4',
+        400: '#6ca9a4',
+        500: '#6f9995',
+      },
+    },
+  },
   variants: {},
   plugins: [],
 };
